0c0f9b606f44bf6c92a7627315e0a60d294f2122
bakuchi- <helveticaf0015se@gmail.com>
2021-04-04 12:47:40 +0900

    fix

--- test/test.ts
+++ test/test.ts
@@ -1,22 +1,22 @@
-import { expect, use } from "chai";
+import { expect } from "chai";
 import { ethers, upgrades } from 'hardhat';
 const toWei = ethers.utils.parseEther
 
-// use(require("chai-bignumber"));
 
 describe("Logic", function () {
     it('test upgrade', async () => {
         const Logic = await ethers.getContractFactory("LogicV1");
         const LogicV2 = await ethers.getContractFactory("LogicV2");
-
+        // deploy
         const instance = await upgrades.deployProxy(Logic, [100]);
 
         await instance.setValue(toWei("150"), toWei("10"));
-        expect((await instance.value()).toString()).to.equal(toWei("160").toString());
+        expect((await instance.value())).to.equal(toWei("160"));
 
+        // upgrade
         const upgraded = await upgrades.upgradeProxy(instance.address, LogicV2);
-        await upgraded.setValue(toWei("200"), toWei("10"));
 
-        expect((await upgraded.value()).toString()).to.equal(toWei("410").toString());
+        await upgraded.setValue(toWei("200"), toWei("10"));
+        expect((await upgraded.value())).to.equal(toWei("410"));
     });
 });
